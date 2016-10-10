32e8b508d70d0429468af74e3930a311e0e45ebd
DrMabuse23 <p.brewing@mwaysolutions.com>
2016-10-10 09:37:57 +0200

    add error Relution

--- relution-workflow/client/src/providers/config.ts
+++ relution-workflow/client/src/providers/config.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
-import { init } from './../../node_modules/relution-sdk/lib/core/init';
+import * as Relution from 'relution-sdk';
+// import { init } from './../../node_modules/relution-sdk/lib/core/init';
 const ICON_PATH = 'assets/icon/';
 
 @Injectable()
@@ -26,7 +27,7 @@ export class StaticUsers {
   ];
 
   constructor(){
-    init({
+    Relution.init({
       serverUrl: 'http://192.168.99.100:8080',
       application: 'workflow-app'
     })
